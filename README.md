cf6d9edee0ef27510e7a3665ecf29f33cc3152a5
Rosiel Victor <codepremium917@gmail.com>
2022-03-22 10:04:20 -0300

    CPF Generator and Validator

---
+++ README.md
@@ -0,0 +1,34 @@
+# Gerador e validador de CPF
+Esse é um simples gerador e validador de CPFs para te ajudar em suas aplicações PHP.
+### Requisitos
+- PHP 7 ou superior
+- Composer (Recomendado)
+## Instalação
+**OBS:** Composer não é obrigatório
+```php
+<?php
+// Instalação simples
+// require 'src/CPF.php';
+
+// Instalação com composer
+require __DIR__. '/vendor/autoload.php';
+
+use Misterioso013\Tools\CPF;
+
+// Gerar um CPF válido aleatórios formatado (123.456.789-10)
+echo CPF::cpfRandom();
+
+// Gerar um CPF válido aleatórios sem máscara (12345678910)
+var_dump(CPF::cpfRandom(0));
+
+
+// Verificar se o CPF é válido retorna true ou false
+echo CPF::validateCPF('12345678910');
+
+// Exemplo de uso
+$cpf = CPF::cpfRandom();
+echo CPF::validateCPF($cpf) ? "O CPF: $cpf é válido!" : "$cpf não é um CPF válido!";
+```
+Esse projeto é bem simples e leve, pode ser usado em qualquer aplicação PHP independente do seu tamanho.
+
+Pretendo desenvolver mais projetos como esse em breve, se você tiver alguma ideia de algo legal que possa ser feito, por favor, deixe-me saber [aqui](https://github.com/misterioso013#conecte-se-comigo).
---
+++ src/CPF.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Misterioso013\Tools;
+
+class CPF
+{
+    /**
+     * Método valida o CPF e retorna true em caso de sucesso
+     * e false no caso do CPF não está correto
+     * @param int|null $cpf
+     * @return boolean
+     */
+    public static function validateCPF(int $cpf = null): bool
+    {
+
+        if (empty($cpf)) {
+            return false;
+        }
+        $cpf = preg_replace("/[^0-9]/", "", $cpf);
+        $cpf = str_pad($cpf, 11, '0', STR_PAD_LEFT);
+
+        if (strlen($cpf) != 11) {
+            return false;
+        } else if (
+            $cpf == '00000000000' ||
+            $cpf == '11111111111' ||
+            $cpf == '22222222222' ||
+            $cpf == '33333333333' ||
+            $cpf == '44444444444' ||
+            $cpf == '55555555555' ||
+            $cpf == '66666666666' ||
+            $cpf == '77777777777' ||
+            $cpf == '88888888888' ||
+            $cpf == '99999999999') {
+            return false;
+            // Calcula os digitos verificadores para verificar se o
+            // CPF é válido
+        } else {
+
+            for ($t = 9; $t < 11; $t++) {
+
+                for ($d = 0, $c = 0; $c < $t; $c++) {
+                    $d += $cpf[$c] * (($t + 1) - $c);
+                }
+                $d = ((10 * $d) % 11) % 10;
+                if ($cpf[$c] != $d) {
+                    return false;
+                }
+            }
+
+            return (bool)$cpf;
+        }
+    }
+
+    /**
+     * Método para gerar CPF válido, com máscara ou não
+     * @param int $mascara
+     * @return string
+     * @example cpfRandom(0)
+     */
+    public static function cpfRandom($mascara = "1"): string
+    {
+        $n1 = rand(0, 9);
+        $n2 = rand(0, 9);
+        $n3 = rand(0, 9);
+        $n4 = rand(0, 9);
+        $n5 = rand(0, 9);
+        $n6 = rand(0, 9);
+        $n7 = rand(0, 9);
+        $n8 = rand(0, 9);
+        $n9 = rand(0, 9);
+        $d1 = $n9 * 2 + $n8 * 3 + $n7 * 4 + $n6 * 5 + $n5 * 6 + $n4 * 7 + $n3 * 8 + $n2 * 9 + $n1 * 10;
+        $d1 = 11 - (self::mod($d1));
+        if ($d1 >= 10) {
+            $d1 = 0;
+        }
+        $d2 = $d1 * 2 + $n9 * 3 + $n8 * 4 + $n7 * 5 + $n6 * 6 + $n5 * 7 + $n4 * 8 + $n3 * 9 + $n2 * 10 + $n1 * 11;
+        $d2 = 11 - (self::mod($d2));
+        if ($d2 >= 10) {
+            $d2 = 0;
+        }
+        $retorno = '';
+        if ($mascara == 1) {
+            $retorno = '' . $n1 . $n2 . $n3 . "." . $n4 . $n5 . $n6 . "." . $n7 . $n8 . $n9 . "-" . $d1 . $d2;
+        } else {
+            $retorno = '' . $n1 . $n2 . $n3 . $n4 . $n5 . $n6 . $n7 . $n8 . $n9 . $d1 . $d2;
+        }
+        return $retorno;
+    }
+
+    /**
+     * @param $dividendo
+     * @return float
+     */
+    private static function mod($dividendo): float
+    {
+        return round($dividendo - (floor($dividendo / 11) * 11));
+    }
+
+}
